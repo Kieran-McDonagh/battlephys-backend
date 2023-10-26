3916546f386815879ce244609f99f6034e299eea
kieran <kieran-mcdonagh@outlook.com>
2023-10-26 19:52:28 +0100

    adds cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const mongooseConnection = require("./connection");
 const apiRouter = require("./routers/api-router");
@@ -8,6 +9,7 @@ const {
   handleMongoErrors,
 } = require("./controllers/errorController");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
