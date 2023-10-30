f8896eb177987ccb4d41577a6f531a2c4fc4d70b
kieran <kieran-mcdonagh@outlook.com>
2023-10-30 15:32:25 +0000

    adds /users/:id/workouts mvp endpoints

--- data/devData/users.js
+++ data/devData/users.js
@@ -1,14 +1,14 @@
 const users = [
-  {
-    username: "kieran",
-    goal: "lose fat",
-    weight: 92,
-    height: 181,
-    age: 27,
-    sex: "M",
-    calories: 0,
-    admin: true,
-  },
+  // {
+  //   username: "kieran",
+  //   goal: "lose fat",
+  //   weight: 92,
+  //   height: 181,
+  //   age: 27,
+  //   sex: "M",
+  //   calories: 0,
+  //   admin: true,
+  // },
 ];
 
 module.exports = users;
